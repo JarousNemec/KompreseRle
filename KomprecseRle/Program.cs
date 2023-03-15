8ca6185b1a1c52e163c3c530493830c04b420a57
JarousNemec <jarous.nemec@gmail.com>
2023-03-15 14:36:03 +0100

    Initial commit

---
+++ KomprecseRle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KomprecseRlel
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            // string input = "aaabb555ccccccd";
+            // string filtered = Rle.RemoveDigits(input);
+            // string output = Rle.Compress(filtered);
+            // Console.WriteLine(input);
+            Rle.CompressFile("rle.txt");
+            Rle.DecompressFile("compressed-rle.txt");
+            // Console.WriteLine(output);
+
+            // string decompressed = Rle.Decompress(output);
+            // Console.WriteLine(decompressed);
+        }
+    }
+}
---
+++ KomprecseRle/Rle.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace KomprecseRlel
+{
+    public class Rle
+    {
+        public static string Compress(string input)
+        {
+            string output = String.Empty;
+
+            uint counter = 0;
+            char previous = input[0];
+            foreach (var c in input)
+            {
+                if (previous == c)
+                {
+                    counter++;
+                }
+                else
+                {
+                    output += $"{counter}{previous}";
+                    previous = c;
+                    counter = 1;
+                }
+            }
+
+            output += $"{counter}{previous}";
+
+            return output;
+        }
+
+        public static string RemoveDigits(string input)
+        {
+            // string output = string.Empty;
+            // foreach (var c in input)
+            // {
+            //     if (!char.IsDigit(c))
+            //     {
+            //         output += c;
+            //     }
+            // }
+            // return output;
+            return Regex.Replace(input, @"\d", "");
+        }
+
+        public static string Decompress(string input)
+        {
+            var output = string.Empty;
+            var number = string.Empty;
+            foreach (var c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    number += c.ToString();
+                }
+                else
+                {
+                    var count = Convert.ToInt32(number);
+                    number = string.Empty;
+                    output += new string(c, count);
+                }
+            }
+
+            return output;
+        }
+
+        public static void CompressFile(string file)
+        {
+            var lines = File.ReadAllLines(file);
+            List<string> output = new List<string>();
+            foreach (var line in lines)
+            {
+                var filtered = RemoveDigits(line);
+                output.Add(Compress(filtered));
+            }
+
+            File.WriteAllLines("compressed-" + file, output);
+        }
+        
+        public static void DecompressFile(string file)
+        {
+            var lines = File.ReadAllLines(file);
+            List<string> output = new List<string>();
+            foreach (var line in lines)
+            {
+                output.Add(Decompress(line));
+            }
+
+            File.WriteAllLines("decompressed-" + file, output);
+        }
+    }
+}
